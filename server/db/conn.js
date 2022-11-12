821ec353aa674fc636ce1030077690e6dc59a785
nisone <nuhu94@gmail.com>
2022-11-12 02:43:33 +0100

    started making changes to project connection

--- server/db/conn.js
+++ server/db/conn.js
@@ -2,23 +2,33 @@ const admin = require("firebase-admin");
 const firestore  = require('firebase-admin/firestore');
 
 const serviceAccount = require("./serviceAccountKey.json");
+const KDYEFServiceAccount = require("./KDYEFServiceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
+const kdyef = admin.initializeApp({
+  credential: admin.credential.cert(KDYEFServiceAccount),
+}, 'kdyef');
+
 const db = firestore.getFirestore();
+const kdyefDb = firestore.getFirestore(kdyef);
 
 async function testFirebase(){
    try {
-        const snapshot = await db.collection('test').doc('test').set({data:'Test'})
-        if (snapshot){
-            console.log('Google fire-store Connected Successfully😀')
-        }
+    console.log('writing to default firestore');
+    await db.collection('test').doc('nisonet-rest-api-test-doc2').set({title: "Hello, Test!"});
+    console.log('writing to default firestore');
+    await kdyefDb.collection('test').doc('kdyef-test-doc2').set({title: "Hello, Test!"});
+    const snapshot = await db.collection('test').doc('test').set({data:'Test'})
+    if (snapshot){
+        console.log('Google fire-store Connected Successfully😀')
+    }
    } catch (error) {
      console.log(error);
         console.log('Google fire-store Connection Failed, Please Check your settings')
    }
 }
 
-module.exports = {admin, db, testFirebase}
+module.exports = {admin, db, kdyefDb, testFirebase}
