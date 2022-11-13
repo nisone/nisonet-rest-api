0e9659b971b72def5ee880cfe90b25f130ca8f99
nisone <nuhu94@gmail.com>
2022-11-13 21:21:03 +0100

    db conn commented sections 2

--- server/db/conn.js
+++ server/db/conn.js
@@ -3,27 +3,27 @@ const admin = require("firebase-admin");
 const firestore  = require('firebase-admin/firestore');
 
 const serviceAccount = JSON.parse(process.env.NISONET_FIREBASE_CONFIG);
-const KDYEFServiceAccount = JSON.parse(process.env.KDYEF_FIREBASE_CONFIG);
+// const KDYEFServiceAccount = JSON.parse(process.env.KDYEF_FIREBASE_CONFIG);
 
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
-const kdyef = admin.initializeApp({
-  credential: admin.credential.cert(KDYEFServiceAccount),
-}, 'kdyef');
+// const kdyef = admin.initializeApp({
+//   credential: admin.credential.cert(KDYEFServiceAccount),
+// }, 'kdyef');
 
 const db = firestore.getFirestore();
-const kdyefDb = firestore.getFirestore(kdyef);
+// const kdyefDb = firestore.getFirestore(kdyef);
 
 async function testFirebase(){
    try {
     console.log('writing to default firestore');
     const r1 = await db.collection('test').doc('nisonet-rest-api-test-doc').set({title: "Hello, Test!"});
-    console.log('writing to default firestore');
-    const r2 = await kdyefDb.collection('test').doc('kdyef-test-doc').set({title: "Hello, Test!"});
-    if (r1 && r2){
+    // console.log('writing to default firestore');
+    // const r2 = await kdyefDb.collection('test').doc('kdyef-test-doc').set({title: "Hello, Test!"});
+    if (r1){
         console.log('Google fire-store Connected Successfully😀')
     }
    } catch (error) {
@@ -32,4 +32,4 @@ async function testFirebase(){
    }
 }
 
-module.exports = {admin, db, kdyefDb, testFirebase}
+module.exports = {admin, db, testFirebase}
