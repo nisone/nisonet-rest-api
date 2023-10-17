19be2843e98b31596dca8a880a272bf8006d2757
nisone <nuhu94@gmail.com>
2023-10-17 20:14:28 +0100

    verify test webhook hash

--- server/routes/webhook.js
+++ server/routes/webhook.js
@@ -109,10 +109,10 @@ router.get('/update-users', (req, res) => {
 });
 
 router.post("/test/transaction/verify", function (req, res) {
-    // const hash = crypto.createHmac('sha512', process.env.PAYSTACK_TEST_SK).update(JSON.stringify(req.body)).digest('hex');
-    // if (hash != req.headers['x-paystack-signature']) {
-    //     return res.sendStatus(403);
-    // }
+    const hash = crypto.createHmac('sha512', process.env.PAYSTACK_TEST_SK).update(JSON.stringify(req.body)).digest('hex');
+    if (hash != req.headers['x-paystack-signature']) {
+        return res.sendStatus(403);
+    }
     const { event, data } = req.body;
 
     if (event == 'charge.success') {
