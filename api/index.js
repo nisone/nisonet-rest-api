b7a278bb85b8bcae2a2d56724edd2db4f7a75185
nisone <nuhu94@gmail.com>
2024-05-24 12:07:26 +0100

    fix vercel api

---
+++ api/index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const cors = require('cors');
+
+require('dotenv').config()
+
+const webhook = require('./../server/routes/webhook.js');
+const usersRoute = require('./../server/routes/users.js');
+const paymentRoute = require('./../server/routes/payment.js');
+const notification = require('./../server/routes/notification.js');
+const vtuRoute = require('./../server/routes/vtu.js');
+const { testFirebase } = require('./../server/db/conn.js');
+const maskawaVTURoute = require('./../server/routes/maskawa-vtu-route.js');
+const wallet = require('./../server/routes/wallet.js');
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(helmet());
+app.use(bodyParser.json({ extended: false }));
+
+app.disable('x-powered-by');
+
+app.get('/', (req, res) => {
+    testFirebase().then(() => {
+        console.log('firebase connection test successful!');
+    }).catch((e) => {
+        console.log('firebase connection test failed!');
+    });
+    res.status(200).json({
+        "message": "welcome"
+    });
+});
+
+app.get('/heartbeat', (req, res) => {
+    res.sendStatus(200);
+});
+
+app.use('/notification', notification);
+app.use('/webhook', webhook);
+app.use('/payment', paymentRoute);
+app.use('/user', usersRoute);
+app.use('/vtu', vtuRoute);
+app.use('/maskawa-vtu', maskawaVTURoute);
+app.use('/wallet', wallet);
+
+app.listen(port, () => {
+    console.log(`App listening on port: ${port}`);
+}).on('connection', (stream) => {
+    console.log(`connection FROM:${stream.address().address} on PORT:${stream.address().port}`);
+}).on('error', (error) => {
+    console.log(`Server Error: ${error}`);
+});
+
+module.exports = app
